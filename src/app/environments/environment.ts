b9317cd63ef19ae720f3948ffc46a2d11ddd7365
annabiInsy2s <mannabi@insy2s.fr>
2024-06-02 18:48:31 +0100

    menu,dish,login

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   env_name: 'dev',
   production: false,
-  apiKey: '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',
+
 
   urlBack: 'http://localhost:9090/api',
 
